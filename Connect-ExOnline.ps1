48cda54ca4635adcd690ee1db77f4079a8ced8f2
Fabian Alberto Campo <fcampo@hotmail.com>
2019-10-09 12:44:03 -0500

    Update Connect-ExOnline.ps1

--- Connect-ExOnline.ps1
+++ Connect-ExOnline.ps1
@@ -1,5 +1,5 @@
 ﻿Function Connect-ExOnline{
-$cred= get-credential -Credential Licencias@BolsaMercantil.com.co
+$cred= get-credential -Credential fabian.campo@campohenriquez.com
 
 Write-Output "Getting the Exchange Online Cmdlets"
 $session= New-PSSession -ConnectionUri https://ps.outlook.com/powershell -ConfigurationName Microsoft.Exchange -Credential $cred -Authentication Basic -AllowRedirection
